527ff7166580b13de11aca6859e1a85777e3c004
Андрей Кумыков <aakumykov@yandex.ru>
2016-10-29 22:38:26 +0700

    Компонент с включённым в него фильтром.

--- index.js
+++ index.js
@@ -16,3 +16,18 @@ app.controller('MyCtrl', ['reverseFilter', function(reverseFilter){
 	this.name = 'Фильтры';
 	this.string1 = '';
 }]);
+
+app.component('reverseArea', {
+	template: `
+		<div>Переворот</div>
+		<ul>
+			<li>name: {{ ctrl2.name }}</li>
+			<li>reversedName: {{ ctrl2.reversedName }}</li>
+		</ul>
+	`,
+	controller: ['reverseFilter', function(reverseFilter){
+		this.name = 'внутренний контроллер reverseArea';
+		this.reversedName = reverseFilter(this.name);
+	}],
+	controllerAs: 'ctrl2',
+});
