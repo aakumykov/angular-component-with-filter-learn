6382dbdd0bfc5bde81accc7a6f95313c39c03b9c
Андрей Кумыков <aakumykov@yandex.ru>
2016-10-30 00:23:11 +0700

    Фильтр втиснут в контроллер компонента.

--- index.js
+++ index.js
@@ -4,32 +4,23 @@ var app = angular.module('MyApp',[]);
 
 app.filter('reverse', function(){
 	return function(str){
-		if (!str) return '';
 		var newStr = '';
-		for (var i=(str.length-1); i>=0; i--) {
-			newStr += str[i];
-		}
+		for (var i=(str.length-1); i>=0; i--) { newStr += str[i]; }
 		return newStr;
 	}
-});
-
-app.controller('MyCtrl', function(){
-	this.name = 'Фильтры';
 });
 
 app.component('reverseArea', {
 	template: `
 		<fieldset>
 			<legend>Reversing area</legend>
-			<lebel>Enter some text:<br>
-			<input type='text' ng-model="ctrl2.directWord">
-			<p><b>{{ ctrl2.directWord | reverse }}</b></p>
+			<p>direct word: {{ctrl.directWord}}</p>
+			<p>reversed word: <i>{{ ctrl.reversedWord }}</i></p>
 		</fieldset>
 	`,
-	controller: function(){
-		//this.directWord;
-		this.directWord = '123';
-		//this.reversedWord = reverseFilter(this.directWord);
-	},
-	controllerAs: 'ctrl2',
+	controller: ['reverseFilter', function(reverseFilter){
+		this.directWord = '12345';
+		this.reversedWord = reverseFilter(this.directWord);
+	}],
+	controllerAs: 'ctrl',
 });
