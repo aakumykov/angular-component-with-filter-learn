cf78d7f36c4be2c7e35e8392b52d33a104edc205
Андрей Кумыков <aakumykov@yandex.ru>
2016-10-30 12:05:11 +0700

    Шаблон компонента во внешнем файле. Работает.

--- component.js
+++ component.js
@@ -2,13 +2,7 @@
 
 angular.module('ReverseArea',[]).
 component('reverseArea', {
-	template: `
-		<fieldset>
-			<legend>Reversing area</legend>
-			<p>original word: {{ctrl.directWord}}</p>
-			<p>filtered word: <i>{{ ctrl.reversedWord }}</i></p>
-		</fieldset>
-	`,
+	templateUrl: 'component-template.html',
 	controller: ['reverseFilter', function(reverseFilter){
 		this.directWord = '12345';
 		this.reversedWord = reverseFilter(this.directWord);
