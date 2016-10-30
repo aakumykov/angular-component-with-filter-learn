ea2605b6821979213f5f0b5e35d8cd527af85832
Андрей Кумыков <aakumykov@yandex.ru>
2016-10-30 12:43:25 +0700

    Улучшил описание.

--- controller.js
+++ controller.js
@@ -3,5 +3,5 @@
 angular.module('MyApp').
 controller('MyCtrl', function(){
 	this.title = 'AngularJS component and filter';
-	this.heading = 'Компонент с контроллером, определённым «снаружи».';
+	this.heading = 'Много файлов. Компонент с контроллером, определённым «снаружи».';
 });
